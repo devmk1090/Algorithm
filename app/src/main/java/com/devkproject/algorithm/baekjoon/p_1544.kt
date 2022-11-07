49a19caf8b86233d596968b6ed5530baef060ee0
devmk1090 <devmk1090@gmail.com>
2022-11-08 00:45:55 +0900

    1544, 1977 solved

---
+++ app/src/main/java/com/devkproject/algorithm/baekjoon/p_1544.kt
@@ -0,0 +1,39 @@
+package com.devkproject.algorithm.baekjoon
+
+fun main() {
+    val mSet = mutableSetOf<String>() //집합처럼 중복되지 않는 고유한 원소만을 가짐
+    val n = readLine()!!.toInt()
+
+    when (n) {
+        0, 1 -> println(n)
+        else -> {
+            repeat(n) { n -> //문자열 or 함수를 주어진 횟수만큼 반복
+                if (n == 0) {
+                    val temp = readLine()
+                    mSet.add(temp!!) //처음 입력받는 단어이기 때문에 1개 카운트
+                } else {
+                    val str = readLine()
+                    val strLength = str!!.length
+                    var checkNew = true
+
+                    if (mSet.map { it.length }.contains(strLength)) { //기존에 저장된 단어와 길이가 같다면
+                        for (i in 0 until strLength) {
+                            //앞자리수를 뒤로 붙이면서 검사
+                            val temp = str.substring(i until strLength) + str.substring(0 until i)
+
+                            if (mSet.contains(temp)) { //똑같은 단어로 판별되면 break
+                                checkNew = false
+                                break
+                            }
+                        }
+                    }
+
+                    if (checkNew) { //새로운 단어라면
+                        mSet.add(str)
+                    }
+                }
+            }
+            println(mSet.size)
+        }
+    }
+}
---
+++ app/src/main/java/com/devkproject/algorithm/baekjoon/p_1977.kt
@@ -0,0 +1,42 @@
+package com.devkproject.algorithm.baekjoon
+
+import kotlin.math.sqrt
+
+fun main() {
+    val m = readLine()!!.toInt()
+    val n = readLine()!!.toInt()
+
+    for (num in solve(m, n)) {
+        println(num)
+    }
+}
+
+private fun solve(m: Int, n: Int): IntArray {
+    val result = IntArray(2) //배열 선언
+    val numList = ArrayList<Int>()
+
+    var sqrt = sqrt(m.toFloat()).toInt() //제곱근 float > int 변환
+    if (sqrt * sqrt < m) {
+        sqrt++ //제곱근값 1 증가시킨후 토스
+    }
+
+    while (true) {
+        val num = sqrt *sqrt
+        if (num > n) {
+            break
+        }
+        numList.add(num) //제곱근 리스트에 할당
+        sqrt++
+    }
+
+    if (numList.size == 0) {
+        return IntArray(1) {
+            -1
+        }
+    }
+
+    result[0] = numList.sum() //리스트에 들어있는 값의 합
+    result[1] = numList[0] //맨처음 할당한 최소값
+
+    return result
+}
