d17dfc4cf4d58f93fe8556f5d9474c27261b6942
devmk1090 <devmk1090@gmail.com>
2022-12-09 01:30:08 +0900

    Pass 2630 problem

--- app/src/main/java/com/devkproject/algorithm/dnc/p_2630.kt
+++ app/src/main/java/com/devkproject/algorithm/dnc/p_2630.kt
@@ -13,16 +13,54 @@ var blue = 0
 var board: Array<IntArray> = arrayOf()
 
 fun main() {
-    val br = System.`in`.bufferedReader()
     val n = readLine()!!.toInt()
 
     //Array(rows) { IntArray(cols) }
     board = Array(n) { IntArray(n) }
 
+    //input 2d array data
     repeat(n) { idx ->
         board[idx] = readLine()!!.split(" ").map { it.toInt() }.toIntArray()
     }
-    for (row in board) {
-        println(row.contentToString())
+
+//    for (row in board) {
+//        println(row.contentToString())
+//    }
+
+    partition(0, 0, n)
+
+    println(white)
+    println(blue)
+}
+
+private fun partition(row: Int, col: Int, size: Int) {
+
+    if (colorCheck(row, col, size)) {
+        if (board[row][col] == 0) {
+            white ++
+        } else {
+            blue ++
+        }
+        return
     }
+    val newSize = size / 2
+
+    //재귀호출
+    partition(row, col, newSize) //2사분면
+    partition(row, col + newSize, newSize) //1사분면
+    partition(row + newSize, col, newSize) //3사분면
+    partition(row + newSize, col + newSize, newSize) //4사분면
+}
+
+private fun colorCheck(row: Int, col: Int, size: Int): Boolean {
+    val color = board[row][col]
+
+    for (i in row until row + size) {
+        for (j in col until col + size) {
+            if (board[i][j] != color) {
+                return false
+            }
+        }
+    }
+    return true
 }
