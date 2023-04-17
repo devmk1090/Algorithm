a4bf0920c154d3b9cb67af37348bd27aa264b916
devmk1090 <devmk1090@gmail.com>
2023-04-18 01:32:35 +0900

    11055 (가장 큰 증가하는 부분 수열) (dp)

---
+++ app/src/main/java/com/devkproject/algorithm/dp/p_11055.kt
@@ -0,0 +1,23 @@
+package com.devkproject.algorithm.dp
+
+import kotlin.math.max
+
+/**
+ * Dynamic Programming(동적 프로그래밍)
+ * https://www.acmicpc.net/problem/11055 (가장 큰 증가하는 부분 수열)
+ */
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    val list = readLine().split(" ").map { it.toInt() }
+    val dp = Array(n) { 0 }
+
+    for (i in list.indices) {
+        dp[i] = max(dp[i], list[i])
+        for (j in i + 1 until list.size) {
+            if (list[i] >= list[j]) continue //다음 수가 작으면 pass
+            dp[j] = max(dp[j], dp[i] + list[j])
+        }
+    }
+    println(dp.maxOrNull())
+}
