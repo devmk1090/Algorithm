0922c38cde453ffd063dcf7b7ed17ddb82f13660
devmk1090 <devmk1090@gmail.com>
2023-04-25 01:51:12 +0900

    Lv.0_120808_분수의 덧셈

---
+++ app/src/main/java/com/devkproject/algorithm/lv_0/p_120808.kt
@@ -0,0 +1,45 @@
+package com.devkproject.algorithm.lv_0
+
+/**
+ * Programmers Lv.0
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120808
+ * 분수의 덧셈
+ */
+
+//유클리드 호제법
+//최대공약수 : 비교할 두 수를 계속 나누어 나머지가 0이되면 최대공약수
+//최소공배수 : 입력받은 두 수(a, b)의 곱에서 최대공약수를 나눈다
+
+fun main() {
+    solution(2, 1, 4, 3)
+}
+
+fun solution(denum1: Int, num1: Int, denum2: Int, num2: Int): IntArray {
+    val gcd = gcd(num1, num2)
+    val lcm = lcm(num1, num2, gcd)
+
+    val mul1 = lcm / num1
+    val mul2 = lcm / num2
+
+    val denum = denum1*mul1 + denum2*mul2
+    val gcd2 = gcd(denum, lcm)
+
+    return intArrayOf(denum / gcd2, lcm / gcd2)
+}
+
+//최대공약수
+fun gcd(a: Int, b: Int): Int {
+    val maximum = a.coerceAtLeast(b)
+    val minimum = a.coerceAtMost(b)
+
+    return if (minimum == 0) {
+        a.coerceAtLeast(b)
+    } else {
+        gcd(minimum, maximum % minimum)
+    }
+}
+
+//최소공배수
+fun lcm(num1: Int, num2: Int, gcd: Int): Int {
+    return num1 * num2 / gcd
+}
