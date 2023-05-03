2aed3ecb972750b4ac99690426c106d245615879
devmk1090 <devmk1090@gmail.com>
2023-05-04 00:31:22 +0900

    Lv.0_120884_치킨 쿠폰

---
+++ app/src/main/java/com/devkproject/algorithm/lv_0/p_120884.kt
@@ -0,0 +1,18 @@
+package com.devkproject.algorithm.lv_0
+
+/**
+ * Programmers Lv.0
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120884
+ * 치킨 쿠폰
+ */
+
+fun p120884(chicken: Int): Int {
+    var answer = 0
+    var coupon = chicken
+
+    while (coupon >= 10) {
+        answer += coupon / 10
+        coupon = coupon / 10 + coupon % 10
+    }
+    return answer
+}
