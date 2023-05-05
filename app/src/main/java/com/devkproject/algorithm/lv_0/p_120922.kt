d501e14c49e0c932825206d28e11f55b1e93c3f3
devmk1090 <devmk1090@gmail.com>
2023-05-06 00:35:08 +0900

    Lv.0_120922_종이 자르기

---
+++ app/src/main/java/com/devkproject/algorithm/lv_0/p_120922.kt
@@ -0,0 +1,11 @@
+package com.devkproject.algorithm.lv_0
+
+/**
+ * Programmers Lv.0
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120922
+ * 종이 자르기
+ */
+
+fun solution(M: Int, N: Int): Int {
+    return M * N - 1
+}
